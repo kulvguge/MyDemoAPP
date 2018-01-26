f92874863b718c716a0508f00a61171fb4d73efd
jinvovo <skj@jinvovo.com>
2018-01-26 11:46:03 +0800

    Rejected commit: Default

--- app/src/main/java/android/com/mydemoapp/MainActivity.java
+++ app/src/main/java/android/com/mydemoapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     ImageView imageView;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         imageView.setOnClickListener(this);
         ClipDrawable drawable = (ClipDrawable) imageView.getBackground();
         drawable.setLevel(5000);
+        TextView textView=new TextView(this);
     }
 
     @Override
---
+++ app/src/main/java/android/com/mydemoapp/SSSS.java
@@ -0,0 +1,11 @@
+package android.com.mydemoapp;
+
+import java.io.Serializable;
+
+/**
+ * Created by ts on 2018/1/26.
+ */
+
+public class SSSS implements Serializable {
+    public String aaa;
+}
